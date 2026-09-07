90d19253d9429e87848a5752fa3f5fc3c4df014f
benjaLaRacha <benjaf12@gmail.com>
2026-09-07 15:24:43 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+// Guarda la app en el teléfono para que abra sin señal.
+const CACHE = 'antihelada-v1';
+const ARCHIVOS = ['./index.html','./manifest.json','./icono-192.png','./icono-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARCHIVOS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks =>
+    Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  // nunca cachear los envíos a la planilla
+  if(url.hostname.includes('script.google')) return;
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      const copia = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copia)).catch(()=>{});
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
